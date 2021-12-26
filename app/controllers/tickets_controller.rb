d0def4a0c734bc9b8974104e27a421b77474d9f3
YUTA MIZUI <futebol.de.salao10@gmail.com>
2021-12-26 11:38:41 +0900

    Updated event information from external pc

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -16,25 +16,36 @@ class TicketsController < ApplicationController
         user_id: @user.id,
         event_id: @event.id,
       )
-      redirect_to events_path(id: params[:event_id]), notice: "このイベントに　参加予定です"
+      redirect_to events_path(id: params[:event_id]), notice: "このイベントに参加予定です"
     elsif @event.point == 0
       Ticket.create(
         user_id: @user.id,
         event_id: @event.id,
       )
-      redirect_to events_path(id: params[:event_id]), notice: "このイベントに　参加予定です"
+      redirect_to events_path(id: params[:event_id]), notice: "このイベントに参加予定です"
     else
       redirect_to events_path(id: params[:public]), alert: "ポイントが不足しています"
     end
-    
-
   end
 
   def destroy
     @ticket = Ticket.find(params[:ticket_id])
+    @event = Event.find(@ticket.event_id)
+    @user = User.find(@ticket.user_id)
+    
+    ## もしユーザーがポイントを持っていれば、最後のポイントの期限日を反映、ポイントがない場合は１ヶ月後
+    if @user.points.count > 0
+      expired_at = @user.points.last.expired_at
+    else
+      expired_at = Date.today.end_of_day.next_month
+    end
     @ticket.destroy
+    @event.point.times do
+      Point.create(
+        user_id: @user.id,
+        expired_at: expired_at,
+      )
+    end
     redirect_to events_path(type: "joining"), notice: "イベント参加をキャンセルしました。"
-
-
   end
 end
