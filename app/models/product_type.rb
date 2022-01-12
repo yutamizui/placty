d2a266eb6472b7806147220421e8e2291e0b4929
Yuta Mizui <futebol.de.salao10@gmail.com>
2022-01-12 10:09:45 +0900

    Fixed the point rate to 120

--- app/models/product_type.rb
+++ app/models/product_type.rb
@@ -1,5 +1,5 @@
 class ProductType < ActiveHash::Base
     self.data = [
-        {id: 1, name: "通常ポイント購入", price: 100, quantity: 1, plan: 'regular_point_purchase'}, 
+        {id: 1, name: "通常ポイント購入", price: 120, quantity: 1, plan: 'regular_point_purchase'}, 
     ]
   end
