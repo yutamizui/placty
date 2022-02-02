40e1482a77bc494ae8f1ae558c356dc5c777b85a
YUTA MIZUI <futebol.de.salao10@gmail.com>
2022-02-02 19:49:53 +0900

    Updated deploy.rb

--- config/initializers/deploy.rb
+++ config/initializers/deploy.rb
@@ -1,1 +1,6 @@
-set :linked_files, fetch(:linked_files, []).push("config/master.key")
+# set :linked_files, fetch(:linked_files, []).push("config/master.key")
+set :application, "sample_rails_app"
+set :repo_url, "https://github.com/JetBrains/sample_rails_app"
+set :branch, "capistrano-deploy"
+set :deploy_to, "/home/deploy/#{fetch :application}"
+append :linked_files, "config/master.key"
