02cc12c4249d3141518b9974905d265381422a04
YUTA MIZUI <futebol.de.salao10@gmail.com>
2022-02-04 11:53:16 +0900

    Checking how sendgrid works

--- app/models/send_email.rb
+++ app/models/send_email.rb
@@ -1,11 +1,13 @@
 class SendEmail
     require 'sendgrid-ruby'
     include SendGrid
+
+    ## SendEmail.test_email
   
     def self.test_email
       from = Email.new(email: 'info@calahe.com') # SendGridの管理画面でSenderに登録したアドレス
       to = Email.new(email: 'futebol.de.salao10@gmail.com') # 送信したいアドレス
-      subject = 'テストホゲホゲ'
+      subject = 'イベントページへのテスト送信'
       content = Content.new(type: 'text/plain', value: 'ハローハロー')
       mail = Mail.new(from, subject, to, content)
   
