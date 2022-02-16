24667593815985aa9db3ef3caa8afc9abeee9e45
Yuta Mizui <futebol.de.salao10@gmail.com>
2022-02-16 12:34:59 +0900

    Changed the information of sendgrid

--- app/mailers/user_action_mailer.rb
+++ app/mailers/user_action_mailer.rb
@@ -1,18 +1,18 @@
 class UserActionMailer < ApplicationMailer
-    default :from => 'info@calahe.com'
+  default :from => 'info@calahe.com'
 
-    # send a signup email to the user, pass in the user object that   contains the user's email address
-    def inquiry_mail(name, email, title, content)
-      @name = name
-      @email = email
-      @title = title
-      @content = content
-      mail( 
-        :to => "info@calahe.com",
-        :from => @email,
-        :subject => @title,
-        :name => @name,
-        :content => @content
-      )
-    end
+  # send a signup email to the user, pass in the user object that   contains the user's email address
+  def inquiry_mail(name, email, title, content)
+    @name = name
+    @email = email
+    @title = title
+    @content = content
+    mail( 
+      :to => "info@calahe.com",
+      :from => @email,
+      :subject => @title,
+      :name => @name,
+      :content => @content
+    )
   end
+end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,9 +104,7 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :api_key        => ENV['SENDGRID_API_KEY'],
+    :api_key       => ENV['SENDGRID_WEB_API_KEY'],
     :domain         => 'placty.com',
     :enable_starttls_auto => true
   }
