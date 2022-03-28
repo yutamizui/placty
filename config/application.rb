32cacd2b2bdfc82bcf6954cded72d9827f736bde
Yuta Mizui <futebol.de.salao10@gmail.com>
2022-03-28 18:26:46 +0900

    Fixing helper method

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module BoardApp
     config.i18n.available_locales = %i(ja en)
     config.i18n.enforce_available_locales = true
     config.i18n.default_locale = :ja
+    config.action_controller.include_all_helpers = false
 
     I18n.load_path += Dir[Rails.root.join("config/locales/*.{rb,yml}")]
 
