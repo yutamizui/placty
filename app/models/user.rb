c0b8399d27d4be7e55784b443ec76b8df95e736a
YUTA MIZUI <futebol.de.salao10@gmail.com>
2022-09-13 14:26:51 +0900

    Added description of the relationship between users and reports

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,9 @@ class User < ApplicationRecord
   has_many :invoices, dependent: :destroy
   has_many :notice_boards, dependent: :destroy
   has_many :notes, dependent: :destroy
+  has_many :reports, dependent: :destroy
   has_one :bank_account
+
 
   validates :time_zone_id, presence: true
 
